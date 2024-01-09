fcf6062d7dd7c0f1a62c8a383d93ee1a9679929e
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-01-10 00:13:55 +0900

    Create 10-compose.js

---
+++ functional-programming/10-compose.js
@@ -0,0 +1,20 @@
+const compose = (...fns) => (obj) => fns.reduce((res, fn) => fn(res), obj);
+
+const addAB = ({a, b}) => a + b;
+
+const sqare = (factor) => (num) => Math.pow(num, factor);
+
+const dividInteger = (factor) => (num) => Math.floor(num / factor);
+
+const obj = {
+  a: 2,
+  b: 3
+};
+
+const result = compose(
+  addAB,
+  sqare(2),
+  dividInteger(5)
+)(obj);
+
+console.log(result) // 5
